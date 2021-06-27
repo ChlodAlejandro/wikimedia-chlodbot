909996c5df44b71c30ec8a3fab40b75661bd59e6
ChlodAlejandro <chlod@chlod.net>
2021-06-28 00:27:44 +0800

    initial commit; wptc-index

---
+++ README.md
@@ -0,0 +1,21 @@
+# ts-backend-template
+This is a template to be used for backend TypeScript-powered Node.js projects.
+
+This can easily run any TypeScript project meant to run without a user interface.
+This template can be further expanded to include Express or Electron.
+
+This template was built for Node.js v14.15.1.
+
+This template includes the following packages as development dependencies:
+* [`@types/node`](https://npmjs.com/package/@types/node) at version `14.14.41`
+* [`@typescript-eslint/eslint-plugin`](https://npmjs.com/package/@typescript-eslint/eslint-plugin) at version `4.22.0`
+* [`@typescript-eslint/parser`](https://npmjs.com/package/@typescript-eslint/parser) at version `4.22.0`
+* [`eslint`](https://npmjs.com/package/eslint) at version `7.24.0`
+* [`eslint-plugin-import`](https://npmjs.com/package/eslint-plugin-import) at version `2.22.1`
+* [`eslint-plugin-jsdoc`](https://npmjs.com/package/eslint-plugin-jsdoc) at version `32.3.1`
+* [`jest`](https://npmjs.com/package/jest) at version `26.6.3`
+* [`ts-jest`](https://npmjs.com/package/ts-jest) at version `26.5.5`
+* [`ts-node`](https://npmjs.com/package/ts-node) at version `9.1.1`
+* [`typescript`](https://npmjs.com/package/typescript) at version `4.2.4`
+
+This template is released under the Apache License 2.0.
---
+++ src/one-off/WPTCIndex.ts
@@ -0,0 +1,142 @@
+import { ApiResponse, mwn } from "mwn";
+import { VERSION } from "../constants/Constants";
+import project from "../../package.json";
+
+if (!process.env.ENWIKI_USERNAME || !process.env.ENWIKI_PASSWORD) {
+    console.error("missing username or password");
+    process.exit();
+}
+
+(async () => {
+    // API connection
+    const bot = await mwn.init({
+        apiUrl: "https://en.wikipedia.org/w/api.php",
+
+        // TODO: ENVIRONMENT VARIABLES
+        username: process.env.ENWIKI_USERNAME,
+        password: process.env.ENWIKI_PASSWORD,
+
+        userAgent: `ChlodBot ${
+            VERSION
+        } (https://w.wiki/3Yti; chlod@chlod.net; User:Chlod) mwn/${
+            project.dependencies.mwn.replace(/^\^/, "")
+        } node/${
+            process.version.replace(/^v/, "")
+        }`,
+        defaultParams: {
+            assert: "user"
+        }
+    });
+
+    // Enable emergency shutoff
+    bot.enableEmergencyShutoff({
+        page: "User:ChlodBot/WPTC Indexer/shutdown",
+        intervalDuration: 5000,
+        condition: function (pagetext) {
+            return pagetext === "false";
+        },
+        onShutoff: function () {
+            console.log("Shutting down!!!");
+            process.exit();
+        }
+    });
+
+    /**
+     * Pushes pages (converted into their subject page, if the given page was
+     * a talk page) into a given array.
+     * 
+     * @param array The array to push into.
+     * @param query The API response.
+     */
+    function pushPages(array : Set<string>, res : ApiResponse) {
+        Object.values(res?.query?.pages ?? []).forEach(pageObject => {
+            const page = new bot.title((pageObject as Record<string, any>).title);
+            const title = page.isTalkPage() ? 
+                page.getSubjectPage().getPrefixedText() : page.getPrefixedText();
+            array.add(title);
+            // console.log(`:: pushed ${title}`);
+        });
+    }
+    // ########################################################################
+    // Class A: WikiProject template banner transclusions
+    // ########################################################################
+
+    console.log("Performing class A search...");
+    const classA = new Set<string>();
+
+    for await (const res of bot.continuedQueryGen({
+        action: "query",
+        generator: "embeddedin",
+        geititle: "Template:WikiProject Tropical cyclones",
+        geilimit: 500
+    })) {
+        pushPages(classA, res);
+    }
+
+    // ########################################################################
+    // Class B: WikiProject sub-articles
+    // ########################################################################
+
+    console.log("Performing class B search...");
+    const classB = new Set<string>();
+
+    for await (const res of bot.continuedQueryGen({
+        action: "query",
+        generator: "prefixsearch",
+        gpssearch: "Wikipedia:WikiProject Tropical cyclones",
+        gpslimit: 500
+    })) {
+        pushPages(classB, res);
+    }
+
+    // ########################################################################
+    // Class C: Typhoon-related drafts
+    // ########################################################################
+
+    console.log("Performing class C search...");
+    const classC = new Set<string>();
+
+    const searchTargets = [
+        "intitle:/cyclone/",
+        "intitle:/typhoon/",
+        "intitle:/hurricane/",
+        "intitle:/tropical storm/",
+        "intitle:/(tropical|deep) depression/",
+    ];
+
+    for (const searchTarget of searchTargets) {
+        console.log(`:: ${searchTarget}`);
+        for await (const res of bot.continuedQueryGen({
+            action: "query",
+            generator: "search",
+            gsrsearch: searchTarget,
+            gsrnamespace: "118",
+            gsrlimit: 500
+        })) {
+            pushPages(classC, res);
+        }
+    }
+
+    // ########################################################################
+    // Dump to file / save to Wikipedia
+    // ########################################################################
+
+    console.log("Dumping...");
+
+    const sets = {
+        "Class A": classA,
+        "Class B": classB,
+        "Class C": classC
+    };
+
+    for (const [name, set] of Object.entries(sets)) {
+        await bot.save(
+            `User:ChlodBot/WPTC Indexer/${name}`,
+            [...set].map(p => `* {{la2|${p}}}`).join("\n"),
+            `(bot) Updating [[User:ChlodBot/WPTC Indexer|WikiProject Tropical cyclones]] page indexes ([[User:ChlodBot/WPTC Indexer#${name}|${name}]])`
+        );
+    }
+
+    console.log("Done.");
+    bot.disableEmergencyShutoff();
+})();
