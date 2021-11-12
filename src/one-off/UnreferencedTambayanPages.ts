a1ab66f514100dd143c7cf78f17d51cc72e5bc5b
ChlodAlejandro <chlod@chlod.net>
2021-11-12 23:29:48 +0800

    redo SQL

--- src/one-off/UnreferencedTambayanPages.ts
+++ src/one-off/UnreferencedTambayanPages.ts
@@ -3,6 +3,8 @@ import createConnection from "../database/Database";
 import {RowDataPacket} from "mysql2";
 import nd from "../util/nd";
 import iswitch from "../util/iswitch";
+import * as path from "path";
+import * as fs from "fs/promises";
 
 (async () => {
     const { log, bot } = await OneOffTask.create("Unreferenced Tambayan Philippines pages");
@@ -12,38 +14,10 @@ import iswitch from "../util/iswitch";
 
     log.info("Performing query...");
     const start = process.hrtime.bigint();
-    const [ rows ] = await sql.query<RowDataPacket[]>(`
-        SELECT
-            REPLACE(\`page_title\`, "_", " ") AS \`title\`,
-            DATE_FORMAT(\`rev_timestamp\`, "%Y-%m-%d %H:%i:%s") AS \`last_timestamp\`,
-            \`actor_name\` AS \`last_editor\`,
-            \`page_len\` AS \`len\`,
-            (CASE \`cl_to\`
-                WHEN "Unknown-importance_Philippine-related_articles" THEN 0
-                WHEN "Low-importance_Philippine-related_articles" THEN 1
-                WHEN "Mid-importance_Philippine-related_articles" THEN 2
-                WHEN "High-importance_Philippine-related_articles" THEN 3
-                WHEN "Top-importance_Philippine-related_articles" THEN 4
-            END) AS \`importance\`
-        FROM \`categorylinks\` c1
-        JOIN \`page\` p1 ON \`cl_from\` = \`page_id\`
-        JOIN \`revision\` ON \`page_latest\` = \`rev_id\`
-        JOIN \`actor\` ON \`rev_actor\` = \`actor_id\`
-        WHERE
-            (\`cl_to\` = "Top-importance_Philippine-related_articles" OR
-            \`cl_to\` = "High-importance_Philippine-related_articles" OR
-            \`cl_to\` = "Mid-importance_Philippine-related_articles" OR
-            \`cl_to\` = "Low-importance_Philippine-related_articles" OR
-            \`cl_to\` = "Unknown-importance_Philippine-related_articles") AND
-            (SELECT TRUE FROM \`page\` p2
-            JOIN \`categorylinks\` c2 ON p2.\`page_id\` = c2.\`cl_from\`
-            WHERE
-                p2.\`page_namespace\` = 0 AND
-                p2.\`page_title\` = p1.\`page_title\` AND
-                c2.\`cl_to\` = "All_articles_lacking_sources"
-            HAVING COUNT(p2.\`page_id\`))
-        ORDER BY \`Importance\` DESC
-    `);
+    const [ rows ] = await sql.query<RowDataPacket[]>(
+        await fs.readFile(path.resolve(__dirname, "sql", "UnreferencedTambayanPages.sql"))
+            .then(f => f.toString())
+    );
     const end = process.hrtime.bigint();
     log.info(`Query finished. Took ${Number(end - start) / 1000000}ms.`);
     log.info("Closing SQL connection...");
@@ -64,10 +38,10 @@ import iswitch from "../util/iswitch";
     `) + "\n";
     for (const row of rows) {
         wikitext += nd(`
-            | {{la3|${row["title"]}}}
-            | ${row["last_timestamp"]}
-            | {{u|${row["last_editor"]}}}
-            | {{nts|${row["len"]}}}
+            | {{la3|${row["page_title"]}}}
+            | ${row["s_last_edit"]}
+            | {{u|${row["s_last_editor"]}}}
+            | {{nts|${row["s_length"]}}}
             | {{nts|${row["importance"]}|quiet=y}} ${
                 iswitch(row["importance"], {
                     1: "Low",
---
+++ src/one-off/sql/UnreferencedTambayanPages.sql
@@ -0,0 +1,35 @@
+SELECT
+    REPLACE(p1.`page_title`, "_", " ") AS `page_title`,
+    DATE_FORMAT(`rev_timestamp`, "%Y-%m-%d %H:%i:%s") AS `s_last_edit`,
+    `actor_name` AS `s_last_editor`,
+    `page_len` AS `s_length`,
+    (CASE `t_importance`
+         WHEN "Unknown-importance_Philippine-related_articles" THEN 1
+         WHEN "Low-importance_Philippine-related_articles" THEN 2
+         WHEN "Mid-importance_Philippine-related_articles" THEN 3
+         WHEN "High-importance_Philippine-related_articles" THEN 4
+         WHEN "Top-importance_Philippine-related_articles" THEN 5
+         END) AS `importance`
+FROM `page` p1
+JOIN `categorylinks` c1 ON p1.`page_id` = c1.`cl_from`
+JOIN `revision` ON `page_latest` = `rev_id`
+JOIN `actor` ON `rev_actor` = `actor_id`
+JOIN (
+    SELECT
+        `page_title` AS `t_page_title`,
+        c2.`cl_to` AS `t_importance`
+    FROM `categorylinks` c2
+             JOIN `page` p2 ON p2.`page_id` = c2.`cl_from`
+    WHERE
+        p2.`page_namespace` = 1 AND
+        (
+            c2.`cl_to` = "Top-importance_Philippine-related_articles" OR
+            c2.`cl_to` = "High-importance_Philippine-related_articles" OR
+            c2.`cl_to` = "Mid-importance_Philippine-related_articles" OR
+            c2.`cl_to` = "Low-importance_Philippine-related_articles" OR
+            c2.`cl_to` = "Unknown-importance_Philippine-related_articles"
+        )
+) a ON `t_page_title` = `page_title`
+WHERE
+    c1.`cl_to` = "All_articles_lacking_sources"
+ORDER BY `importance` DESC, `s_last_edit` DESC
