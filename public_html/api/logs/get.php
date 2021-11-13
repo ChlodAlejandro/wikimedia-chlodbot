e0f7483805e18368f312051d120bb9f49e4a198d
ChlodAlejandro <chlod@chlod.net>
2021-11-14 00:53:15 +0800

    missed a dir

--- public_html/api/logs/get.php
+++ public_html/api/logs/get.php
@@ -4,7 +4,7 @@ require __DIR__ . "/../../system/get_log_files.php";
 if (in_array($_GET["file"], get_log_files())) {
     header("Content-Type: text/plain");
 
-    $logsPath = __DIR__ . "/../../../logs";
+    $logsPath = __DIR__ . "/../../../../logs";
     echo file_get_contents(realpath($logsPath . DIRECTORY_SEPARATOR . $_GET["file"]));
 } else {
     http_response_code(400);
