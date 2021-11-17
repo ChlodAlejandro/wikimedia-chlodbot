737cd450fa6a35f58e31885be1e58deb85f6642d
ChlodAlejandro <chlod@chlod.net>
2021-11-17 22:46:46 +0800

    undo pain

--- scripts/jsub-one-off.sh
+++ scripts/jsub-one-off.sh
@@ -6,19 +6,15 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
-start() {
-    /usr/bin/jsub \
-        -N "$1" \
-        -o "$HOME/logs/$1.out" \
-        -e "$HOME/logs/$1.err" \
-        -quiet -once -mem 1024m $2 \
-        "$SCRIPT_PATH/one-off.sh" "$1"
-}
+/usr/bin/jsub \
+    -N "$1" \
+    -o "$HOME/logs/$1.out" \
+    -e "$HOME/logs/$1.err" \
+    -quiet -once -mem 1024m \
+    "$SCRIPT_PATH/one-off.sh" "$1"
 
 if [ -t 1 ]; then
-    tail -fqn 0 "$HOME/logs/$1.out" "$HOME/logs/$1.err" & start "$1" "-sync y"
-else
-    start "$1"
+    tail -fqn 0 "$HOME/logs/$1.out" "$HOME/logs/$1.err"
 fi
 
 set +e
