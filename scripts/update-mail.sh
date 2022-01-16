63d5b1ed1088c4676672097caf901df258b01462
ChlodAlejandro <chlod@chlod.net>
2022-01-16 10:50:57 +0800

    fix hash display in email

--- scripts/update-mail.sh
+++ scripts/update-mail.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set +u
 echo "Hello!"
 echo
 echo "A new version of Zoomiebot was deployed on `date`."
 echo
-git log -1
+git log $1..HEAD
 echo
 echo "If this deploy was made in error, please log into Toolforge immediately"
 echo "and rectify the error. Thank you."
+set -u
--- scripts/update.sh
+++ scripts/update.sh
@@ -25,7 +25,7 @@ npm run build
 echo Done! Deployed commit `git rev-parse --short HEAD`.
 
 # Send email to maintainers
-bash scripts/update-mail.sh | mail \
+bash scripts/update-mail.sh $GIT_HASH | mail \
     -r tools.zoomiebot@toolforge.org \
-    -s "New Zoomiebot version deployed [`$GIT_HASH`]" \
+    -s "New Zoomiebot version deployed [$GIT_HASH]" \
     "zoomiebot.maintainers@toolforge.org"
