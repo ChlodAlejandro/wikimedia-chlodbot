78a499ba7b97ceb062cfda613a2b20909903e755
Chlod Alejandro <chlod@chlod.net>
2022-10-08 22:28:21 +0800

    add log rotate script

---
+++ etc/rotate-logs.sh
@@ -0,0 +1,18 @@
+PPWD=`pwd`
+cd $HOME/project/.logs
+for i in *.log
+do
+    if [ -f "$i" ]
+    then
+        NOW=$(date +%Y-%m-%dT%H.%M.%S)
+        BASENAME=$(basename "$i" .log)
+        TEMPNAME="$BASENAME-$NOW.log"
+
+        mkdir -p "$NOW"
+        mv "$i" "$TEMPNAME"
+        touch "$i"
+        gzip "$TEMPNAME" -S.gz
+        mv "$TEMPNAME" "$NOW/$TEMPNAME"
+    fi
+done
+cd $PPWD
